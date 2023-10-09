174bd38a829e146ec76f1e9b8e9868d09022d765
Bikathi Martin <bikathimartin@outlook.com>
2023-10-09 22:06:46 +0300

    First commit

---
+++ src/main/java/npc/bikathi/springstkpush/SpringstkpushApplication.java
@@ -0,0 +1,13 @@
+package npc.bikathi.springstkpush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringstkpushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringstkpushApplication.class, args);
+	}
+
+}
---
+++ src/test/java/npc/bikathi/springstkpush/SpringstkpushApplicationTests.java
@@ -0,0 +1,13 @@
+package npc.bikathi.springstkpush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringstkpushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
