c2db642fad7ef3071b36621be75a2c8ce3d2e92e
Bikathi Martin <bikathimartin@outlook.com>
2023-10-10 16:21:43 +0300

    add proxy to circumvent CORS

--- src/springstkpush-ui/vite.config.js
+++ src/springstkpush-ui/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+	plugins: [vue()],
+	server: {
+		port: 5173,
+		proxy: {
+			"/api/v1/payment/initiate": {
+				target: "http://localhost:8080",
+				ws: true,
+				changeOrigin: true,
+			},
+		},
+	},
+});
